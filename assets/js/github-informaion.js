9d602c23cbc0007bdd5eddf3b05168ee65f503f9
Duncan Falconer <duncan.niamh@gmail.com>
2018-05-08 08:06:34 +0000

    Set up API throttling to display a timer when a user can try log into git, after the rate limit to try access the API is reached

--- assets/js/github-informaion.js
+++ assets/js/github-informaion.js
@@ -64,8 +64,10 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            }
-            else {
+            } else if(errorResponse.status === 403){
+              var resetTime = new Date(errorResponse.getResponseHeader('x-Ratelimit-Reset')*1000);
+               $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()} has passed</h4>`);
+            } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
