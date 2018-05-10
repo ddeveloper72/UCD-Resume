45e22f4a37dc8c296317026d4c227e5b12d861ca
ddeveloper72 <duncan.niamh@gmail.com>
2018-05-10 14:36:35 +0100

    Created .htaccess and 404 and 405 error pages

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -3,14 +3,16 @@ function sendMail(contactForm) {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
-    })
+        })    
     .then(
         function(response) {
+            alert("Sent!");
             console.log("SUCCESS", response);
-        },
+      },
         function(error) {
+            alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
             console.log("FAILED", error);
         }
     );
-    return false;
+    return false;    
 }
